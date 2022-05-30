143ff95c2dfcf9c71cafc874b596e45bb5d7d54e
LifelessNerd <35869162+LifelessNerd@users.noreply.github.com>
2022-05-30 20:43:08 +0200

    nA WERKCOLLEGE

---
+++ Python/HW2/main.py
@@ -0,0 +1,10 @@
+import nltk
+from nltk.chunk.util import conlltags2tree, tree2conlltags
+
+# If numpy is absent, the nltk fails with a very confusing error.
+# We avoid problems by checking directly
+try:
+    import numpy
+except ImportError:
+    print("You need to download and install numpy!!!")
+    raise
--- Python/Mini-assignment-4/alice.py
+++ Python/Mini-assignment-4/alice.py
@@ -1,9 +1,27 @@
 # import nltk.corpus
 # from nltk.book import *
+import re
+from nltk.corpus import gutenberg
 
-from nltk.corpus import alpino
+words = gutenberg.words("carroll-alice.txt")
+words = [w.lower() for w in words if re.search(r'\w', w)]
 
-print(alpino.readme())
 
-allwords = alpino.words()
-print(alpino.words())
+woordenlijst = {}
+
+for index, word in enumerate(words):
+        if word.lower() == 'alice':
+            nextword = words[index + 1]
+            if nextword in woordenlijst.keys():
+                woordenlijst[nextword] = woordenlijst[nextword] + 1
+            else:
+                woordenlijst[nextword] = 1
+
+woordenlijst = dict(sorted(woordenlijst.items(), key=lambda item: item[1], reverse=True))
+
+
+
+if __name__ == "__main__":
+    for index, woord in enumerate(woordenlijst):
+        if index < 5:
+            print(woord, woordenlijst[woord])
