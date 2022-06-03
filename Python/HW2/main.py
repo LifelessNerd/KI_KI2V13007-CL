d226c6c4598e31f7f33f85c30613d6282effd513
LifelessNerd <35869162+LifelessNerd@users.noreply.github.com>
2022-06-03 15:54:03 +0200

    na werkcollege 2/6

--- Python/HW2/main.py
+++ Python/HW2/main.py
@@ -4,6 +4,7 @@ from nltk.chunk.util import conlltags2tree, tree2conlltags
 # If numpy is absent, the nltk fails with a very confusing error.
 # We avoid problems by checking directly
 try:
+
     import numpy
 except ImportError:
     print("You need to download and install numpy!!!")
